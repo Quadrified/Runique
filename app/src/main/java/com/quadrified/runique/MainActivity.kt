c3ac119b79d012e87599b045a9c570b3d0243183
Omer Quadri <omerquadri01@gmail.com>
2024-08-30 11:40:48 -0400

    [5.4] Add MaterialTheme to the app

--- app/src/main/java/com/quadrified/runique/MainActivity.kt
+++ app/src/main/java/com/quadrified/runique/MainActivity.kt
@@ -5,8 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -20,10 +20,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RuniqueTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.primary
+                ) {
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -39,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     RuniqueTheme {
