c9aae0b45d0488cd4b2567a7f8cd3b4ba401679c
Omer Quadri <omerquadri01@gmail.com>
2024-09-02 14:56:57 -0400

    [6.7] Add Token Refresh mechanism

--- core/data/src/main/java/com/quadrified/core/data/networking/HttpClientFactory.kt
+++ core/data/src/main/java/com/quadrified/core/data/networking/HttpClientFactory.kt
@@ -1,8 +1,14 @@
 package com.quadrified.core.data.networking
 
 import com.quadrified.core.data.BuildConfig
+import com.quadrified.core.domain.AuthInfo
+import com.quadrified.core.domain.SessionStorage
+import com.quadrified.core.domain.util.Result
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
@@ -15,7 +21,9 @@ import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import timber.log.Timber
 
-class HttpClientFactory {
+class HttpClientFactory(
+    private val sessionStorage: SessionStorage
+) {
     fun build(): HttpClient {
         return HttpClient(CIO) {
             install(ContentNegotiation) {
@@ -37,6 +45,59 @@ class HttpClientFactory {
             defaultRequest {
                 contentType(ContentType.Application.Json)
                 header("x-api-key", BuildConfig.API_KEY)
+            }
+            install(Auth) {
+                bearer {
+                    // Current token combo
+                    loadTokens {
+                        // Reads current AuthInfo sessionStorage data
+                        val info = sessionStorage.get()
+
+                        BearerTokens(
+                            accessToken = info?.accessToken ?: "",
+                            refreshToken = info?.refreshToken ?: ""
+                        )
+                    }
+
+                    // TOo get new tokens if any expires
+                    refreshTokens {
+                        // Reading existing AuthInfo
+                        val info = sessionStorage.get()
+
+                        // If accessToken expired, make API call to get new accessToken
+                        // Uses current refreshToken and userId to get new accessToken
+                        val response = client.post<AccessTokenRequest, AccessTokenResponse>(
+                            route = "/accessToken",
+                            body = AccessTokenRequest(
+                                refreshToken = info?.refreshToken ?: "",
+                                userId = info?.userId ?: ""
+                            )
+                        )
+
+                        if (response is Result.Success) {
+                            // New data from API response
+                            val newAuthInfo = AuthInfo(
+                                accessToken = response.data.accessToken,
+                                refreshToken = info?.refreshToken ?: "",
+                                userId = info?.userId ?: "",
+                            )
+
+                            // Update sessionStorage with new data
+                            sessionStorage.set(newAuthInfo)
+
+                            // New token
+                            BearerTokens(
+                                accessToken = newAuthInfo.accessToken,
+                                refreshToken = newAuthInfo.refreshToken
+                            )
+                        } else {
+                            BearerTokens(
+                                accessToken = "",
+                                refreshToken = ""
+                            )
+                        }
+                    }
+                }
             }
         }
     }
