e68f7c5edd27853fe19bc4867760b59624390a28
Omer Quadri <omerquadri01@gmail.com>
2024-09-14 20:25:36 -0400

    [10.3] Implement Analytics UI

--- build-logic/convention/src/main/java/com/quadrified/convention/BuildTypes.kt
+++ build-logic/convention/src/main/java/com/quadrified/convention/BuildTypes.kt
@@ -61,6 +61,7 @@ internal fun Project.configureBuildTypes(
                         }
                         release {
                             configureReleaseBuildType(commonExtension, apiKey)
+                            isMinifyEnabled = false
                         }
                     }
                 }
