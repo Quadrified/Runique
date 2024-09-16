402209239a7403f870f2df7f027adbcd5377edb4
Omer Quadri <omerquadri01@gmail.com>
2024-09-16 19:28:40 -0400

    [Wear@3.5] Adding Wear Permissions

--- wear/run/presentation/src/main/java/com/quadrified/wear/run/presentation/TrackerViewModel.kt
+++ wear/run/presentation/src/main/java/com/quadrified/wear/run/presentation/TrackerViewModel.kt
@@ -1,16 +1,51 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
 package com.quadrified.wear.run.presentation
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.quadrified.wear.run.domain.ExerciseTracker
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
-class TrackerViewModel: ViewModel() {
+class TrackerViewModel(
+    private val exerciseTracker: ExerciseTracker
+) : ViewModel() {
 
     var state by mutableStateOf(TrackerState())
         private set
 
+    private val hasBodySensorPermission = MutableStateFlow(false)
+
+
     fun onAction(action: TrackerAction) {
+
+        when (action) {
+            is TrackerAction.OnBodySensorPermissionResult -> {
+                hasBodySensorPermission.value = action.isGranted
+                if (action.isGranted) {
+                    viewModelScope.launch {
+                        val isHeartRateTrackingSupported =
+                            exerciseTracker.isHeartRateTrackingSupported()
+                        state = state.copy(
+                            canTrackHeartRate = isHeartRateTrackingSupported
+                        )
+                    }
+                }
+            }
+
+            TrackerAction.OnFinishRunClick -> {
+                Unit
+            }
+
+            TrackerAction.OnToggleRunClick -> {
+                Unit
+            }
+        }
 
     }
 }
